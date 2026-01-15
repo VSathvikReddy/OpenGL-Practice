b25bc32839a1d80715308f36cbb186c2c8fb1cf2
VSathvikReddy <vangapellisathvik@gmail.com>
2026-01-16 00:04:31 +0530

    Save main

--- src/main.cpp
+++ src/main.cpp
@@ -1,84 +1,108 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+#include "init.h"
+#include "shader.h"
 
-#include <stdlib.h>
-#include <stdio.h>
+#include <iostream>
 #include <math.h>
 
-GLFWwindow* make_window(){
-    GLFWwindow* window;
-
-    /* Initialize the library */
-    if (!glfwInit()){
-        exit(1);
-    }
-
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window){
-        glfwTerminate();
-        exit(1);
-    }
-
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
-
-    return window;
-}
-
-struct Vertex2f{
-    float x;
-    float y;
-
-    Vertex2f():x(0.0f),y(0.0f){}
-    Vertex2f(float _x, float _y):x(_x),y(_y){}
-
-    Vertex2f rotate(float theta = 0.0f){
-        float s = std::sin(theta);
-        float c = std::cos(theta);
-
-        return {
-            x * c - y * s,
-            x * s + y * c
-        };
-    }
-};
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
 
 
-inline void glVertex2f(const Vertex2f& v) {
-    glVertex2f(v.x, v.y);
-}
+void processInput(GLFWwindow *window);
 
 
 int main(){
 
     GLFWwindow* window = make_window();
-    
-    Vertex2f a(0.0f, 0.5f);
-    Vertex2f b(0.5f, -0.5f);
-    Vertex2f c(-0.5f, -0.5f);
-    float angle = 0.0f;
 
-    /* Loop until the user closes the window */
+    unsigned int shaderProgram = init_shader();
+
+
+    float verticesa[] = {
+        // first triangle
+        -0.9f, -0.5f, 0.0f,  // left 
+        -0.0f, -0.5f, 0.0f,  // right
+        -0.45f, 0.5f, 0.0f,  // top 
+
+    };
+    float verticesb[] = {
+        // first triangle
+        -0.9f, -0.5f, 0.0f,  // left 
+        -0.0f, -0.5f, 0.0f,  // right
+        -0.45f, 0.5f, 0.0f,  // top 
+
+    };
+
+
+    unsigned int VBOa, VAOa;
+    glGenVertexArrays(1, &VAOa);
+    glGenBuffers(1, &VBOa);
+    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+    glBindVertexArray(VAOa);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBOa);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(verticesa), verticesa, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+    glBindBuffer(GL_ARRAY_BUFFER, 0); 
+
+    // remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
+    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+    glBindVertexArray(0); 
+
+
+    // uncomment this call to draw in wireframe polygons.
+    //PolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+
     while(!glfwWindowShouldClose(window)){
-        /* Render here */
+        // input
+        // -----
+        processInput(window);
+
+        // render
+        // ------
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
-        glVertex2f(a.rotate(angle));
-        glVertex2f(b.rotate(angle));
-        glVertex2f(c.rotate(angle));
-        glEnd();
 
-        angle += 0.001f;
-
-        /* Swap front and back buffers */
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAOa); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        // glBindVertexArray(0); // no need to unbind it every time 
+ 
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
         glfwSwapBuffers(window);
-
-        /* Poll for and process events */
         glfwPollEvents();
-    }
 
+    }
+    //glfwDestroyWindow(window);
+    // glDeleteVertexArrays(1, &VAO);
+    // glDeleteBuffers(1, &VBO);
+    //glDeleteBuffers(1, &EBO);
+    glDeleteProgram(shaderProgram);
+
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    // ------------------------------------------------------------------
     glfwTerminate();
     return 0;
+}
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
 }
